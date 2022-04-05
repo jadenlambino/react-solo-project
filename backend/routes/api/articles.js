8b964e34b5f09acd38148963954e61a2cb895280
Jaden Lambino <54949187+jadenlambino@users.noreply.github.com>
2022-04-04 22:05:21 -0700

    idk if this works or not

---
+++ backend/routes/api/articles.js
@@ -0,0 +1,12 @@
+// const express = require('express')
+// const asyncHandler = require('express-async-handler')
+// const check = require('express-validator')
+
+// const { Articles } = require('../../db/models')
+// const router = express.Router()
+
+// router.get('/', (req, res) => {
+//     console.log(res)
+// })
+
+// module.exports = router
